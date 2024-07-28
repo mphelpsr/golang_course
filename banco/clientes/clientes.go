c52b2a0883d1b3112e6f41b38838bc86d19e3228
Marcelo Ramalho Oliveira <marcelo.ramalho-oliveira@outlook.com>
2024-07-28 19:19:22 -0300

    estrutura de dados

--- banco/clientes/clientes.go
+++
@@ -1,7 +0,0 @@
-package clientes
-
-type Titular struct {
-	Nome          string
-	CPF           string
-	NomeProfissao string
-}
--- banco/main.go
+++ banco/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"banco/clientes"
-	"banco/contas"
 	"fmt"
 )
 
 func main() {
-	clienteMarcelo := clientes.Titular{}
-	clienteMarcelo.Nome = "Marcelo"
-	clienteMarcelo.CPF = "06237654461"
-	clienteMarcelo.NomeProfissao = "Analista Sistemas"
-	contaMarcelo := contas.ContaCorrente{clienteMarcelo, 222, 12121, 1000.}
 
-	fmt.Println(contaMarcelo)
+	fmt.Println("testando estrutura dados")
 
 }
